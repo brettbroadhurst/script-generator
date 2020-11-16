21939e1e0002d63a1c7c0f2ffc00d4f0af78d7b4
Brett Broadhurst <brettbroadhurst@gmail.com>
2020-11-16 12:05:00 -0800

    Moving stuff around

--- src/index.tsx
+++ src/index.tsx
@@ -4,47 +4,73 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { v4 as uuidv4 } from 'uuid';
 import styles from './styles.module.css';
 import { IDocument } from './types';
 import DocumentList from './containers/DocumentList';
 import './index.css';
 
-const Header: React.FC = () => (
-  <header className={styles.header} />
-);
+type IProps = {
+    handleAddDocument(e: any): void;
+};
+
+const Header: React.FC<IProps> = (props: IProps) => {
+  const { handleAddDocument } = props;
+  return (
+    <header className={styles.header}>
+      <button
+        className={styles.create}
+        type="button"
+        onClick={handleAddDocument}
+      >
+        Add Story
+      </button>
+    </header>
+  );
+};
+
+const docs: IDocument[] = [
+  {
+    id: uuidv4(), title: 'Document 1', createdOn: '', updatedOn: '',
+  },
+  {
+    id: uuidv4(), title: 'Document 2', createdOn: '', updatedOn: '',
+  },
+  {
+    id: uuidv4(), title: 'Document 3', createdOn: '', updatedOn: '',
+  },
+  {
+    id: uuidv4(), title: 'Document 4', createdOn: '', updatedOn: '',
+  },
+  {
+    id: uuidv4(), title: 'Document 5', createdOn: '', updatedOn: '',
+  },
+  {
+    id: uuidv4(), title: 'Document 6', createdOn: '', updatedOn: '',
+  },
+  {
+    id: uuidv4(), title: 'Document 7', createdOn: '', updatedOn: '',
+  },
+  {
+    id: uuidv4(), title: 'Document 8', createdOn: '', updatedOn: '',
+  },
+
+];
 
 const App: React.FC = () => {
-  const documents: IDocument[] = [
-    {
-      id: '1', title: 'Document 1', createdOn: '', updatedOn: '',
-    },
-    {
-      id: '2', title: 'Document 2', createdOn: '', updatedOn: '',
-    },
-    {
-      id: '3', title: 'Document 3', createdOn: '', updatedOn: '',
-    },
-    {
-      id: '4', title: 'Document 4', createdOn: '', updatedOn: '',
-    },
-    {
-      id: '5', title: 'Document 5', createdOn: '', updatedOn: '',
-    },
-    {
-      id: '6', title: 'Document 6', createdOn: '', updatedOn: '',
-    },
-    {
-      id: '7', title: 'Document 7', createdOn: '', updatedOn: '',
-    },
-    {
-      id: '8', title: 'Document 8', createdOn: '', updatedOn: '',
-    },
+  const [documents, setDocuments] = React.useState<IDocument[]>(docs);
 
-  ];
+  function handleAddDocument(e: any) {
+    e.preventDefault();
+
+    setDocuments((prev: IDocument[]) => [...prev, {
+      id: uuidv4(), title: 'New Document', createdOn: new Date().toDateString(), updatedOn: new Date().toDateString(),
+    }]);
+  }
 
   return (
     <div>
-      <Header />
+      <Header handleAddDocument={handleAddDocument} />
       <main>
         <DocumentList documents={documents} />
       </main>
